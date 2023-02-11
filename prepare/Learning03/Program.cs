ac03bbab682a35645c9146707be0e75d4b7d9541
thompsonankomo <nko20005@byui.edu>
2023-02-12 00:31:00 +0200

    commit

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
+public class Fraction
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        private int_top;
+        private int_bottom;
+
+        public fraction()
+        {
+            _top =4;
+            _bottom =5;
+        }
+        public Fraction (int WholeNumber)
+        {
+            _top = WholeNumber;
+            _bottom = 1;
+        }
+        public Fraction (int top , int bottom)
+        {
+            _top = top;
+            _bottom = bottom;
+        }
+        public string GetFractionString()
+        {
+            string text =$"{_top}/ {_bottom}";
+            return text();
+        }
+        
+
     }
 }
