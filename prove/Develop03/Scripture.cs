75f2394f83834b5fbad1b286e506bf27fa10b32e
thompsonankomo <103927597+thompsonankomo@users.noreply.github.com>
2023-03-17 19:51:12 +0200

    commit

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,33 +3,33 @@ public class Scripture
  public Scripture( Reference reference, string scripture)
  {
     _reference = reference;
-    _words= NewWordsList(scripture);
+    _words= WordsList(scripture);
  }
  private Reference _reference;
  private Reference GetReference()
  {
     return _reference;
  }
- public void SetReference(Reference reference)
+ public void SetReference(Reference _reference)
  {
     _reference = reference;
  }
  private List<Words> _words;
  private List<Words> GetList()
  {
-    return Words;
+    return _words;
  }
  private void SetList(string scripture)
  {
-    _words = NewWordsList(scripture);
+    _words = WordsList(scripture);
  }
- private List<Words> NewWordsList(string scripture)
+ private List<Words> WordsList(string scripture)
  {
     foreach(Words in scripture.Split(" "))
     {
-       Scripture.NewWordsList.Add(new Words(word));
+       Scripture.WordsList.Add(new Words(word));
     }
-    return NewWordsList;
+    return WordsList;
  }
  public string Display()
  {
