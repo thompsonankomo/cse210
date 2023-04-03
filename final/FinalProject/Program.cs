b2709e8db73feaf4dec16e14411f379bda41090e
thompsonankomo <nko20005@byui.edu>
2023-04-03 05:41:34 +0200

    commit

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class program
 {
-    static void Main(string[] args)
+    class store
     {
-        Console.WriteLine("Hello FinalProject World!");
+        public string name;
+        public string customerDetails{get; protected set;}
+        public string departments{get; set;}
+        public string menu;
+        public int priceOfProducts{get; set;}
+
+        public store(string name, string customerDetails,string departments, string menu, int priceOfProducts)
+         {
+            this.name = name;
+            this.customerDetails= customerDetails;
+            this.departments=departments;
+            this.menu = menu;
+            this.priceOfProducts= priceOfProducts;
+         }
+         private void setName(string name)
+         {
+            this.name = name;
+         }
+         private void setCustomerDetails(string customerDetails)
+         {
+            this.customerDetails = customerDetails;
+         }
+         public void setDepartments(string departments)
+         {
+            this.departments = departments;
+         }
+         public void setMenu(string menu)
+         {
+            this.menu = menu;
+         }
+
+
+          
+         
+
+    
+
+        
+
+
+        
+
+        
+
+
     }
+
 }
