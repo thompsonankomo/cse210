08c81e6e6468966edfd4c74a590db35a7e41f878
thompsonankomo <nko20005@byui.edu>
2023-04-03 07:07:45 +0200

    commit

---
+++ final/FinalProject/Electricals.cs
@@ -0,0 +1,21 @@
+using System;
+ 
+
+class electricals
+{
+ public List <Products> electricals
+ {
+ 
+ }
+ public void printMenu()
+ {
+    Console.WriteLine("Welcome to Asakhe electrical tools");
+    Console.WriteLine("1.Print all Products");
+    Console.WriteLine("2. Add a Product");
+    Console.WriteLine("3. Search a product");
+    Console.WriteLine("4. Remove a product");
+    Console.WriteLine("5.Save to Cart");
+    Console.WriteLine("6.Exit");
+ }
+}
+ 
---
+++ final/FinalProject/Fitting.cs
@@ -0,0 +1,21 @@
+using System;
+ 
+
+class fittingDepartment
+{
+ public List <Products> fittingDepatment
+ {
+ 
+ }
+ public void printMenu()
+ {
+    Console.WriteLine("Welcome to Asakhe Gardening tools");
+    Console.WriteLine("1.Print all Products");
+    Console.WriteLine("2. Add a Product");
+    Console.WriteLine("3. Search a product");
+    Console.WriteLine("4. Remove a product");
+    Console.WriteLine("5.Save to Cart");
+    Console.WriteLine("6.Exit");
+ }
+}
+ 
